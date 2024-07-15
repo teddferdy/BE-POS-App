f370c8013c61dd77261cc34f20419b66558265b0
Teddy-FN <tedd.ferdy@gmail.com>
2024-07-15 18:47:42 +0700

    FIxing Coba Lagi

--- src/index.js
+++ src/index.js
@@ -9,9 +9,37 @@ import cors from "cors";
 // Error Controller
 
 const app = express();
+
+const whitelist = ["*"];
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  const origin = req.get("referer");
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,Content-Type,Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+  }
+  // Pass to next layer of middleware
+  if (req.method === "OPTIONS") res.sendStatus(200);
+  else next();
+});
+
+const setContext = (req, res, next) => {
+  if (!req.context) req.context = {};
+  next();
+};
+app.use(setContext);
 
 // Set EJS
 // app.set("view engine", "ejs");
