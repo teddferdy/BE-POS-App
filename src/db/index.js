69dabdc22e05e41f87a44bc17716bf2c0323c996
Teddy-FN <tedd.ferdy@gmail.com>
2024-07-15 22:23:57 +0700

    Coba lagi ahhh

--- src/db/index.js
+++ src/db/index.js
@@ -2,15 +2,8 @@ const { Pool } = require('pg')
 require('dotenv').config()
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL_URL,
-  user: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORTING,
-  host: process.env.POSTGRES_HOST,
-  idleTimeoutMillis: 0,
-  connectionTimeoutMillis: 0,
-});
+  connectionString:  process.env.POSTGRES_URL,
+})
 
 module.exports = {
   pool,
