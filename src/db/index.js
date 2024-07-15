67f3a433041e83629bebc95600fa3824854e8b0c
Teddy-FN <tedd.ferdy@gmail.com>
2024-07-15 22:02:34 +0700

    Fixing Broo

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  // connectionString: process.env.POSTGRES_URL,
+  connectionString: process.env.POSTGRES_URL,
   user: "postgres",
   database: "cashier_app",
   password: "teddyferdian98",
