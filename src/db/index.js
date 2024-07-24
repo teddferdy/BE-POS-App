af02274e6e42ef8c6d99dad59c4c1163028b81bb
Teddy-FN <tedd.ferdy@gmail.com>
2024-07-24 16:26:28 +0700

    Adding Feature Auth

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,16 @@
-const { Pool } = require('pg')
-require('dotenv').config()
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  connectionString:  process.env.POSTGRES_URL,
-})
+  connectionString: process.env.POSTGRES_URL,
+  // user: process.env.POSTGRES_USER_LOCAL,
+  // host: process.env.POSTGRES_HOST_LOCAL,
+  // password: process.env.POSTGRES_PASSWORD_LOCAL,
+  // database: process.env.POSTGRES_DATABASE_LOCAL,
+  // port: process.env.POSTGRES_PORTING_LOCAL,
+  // idleTimeoutMillis: 0,
+  // connectionTimeoutMillis: 0,
+});
 
 module.exports = {
   pool,
