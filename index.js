e5bbeb05f8357d30ea15a174c65dff771aa58acb
Teddy-FN <tedd.ferdy@gmail.com>
2024-08-10 20:35:55 +0700

    Add JWT in Express

--- index.js
+++ index.js
@@ -1,53 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const path = require("path");
 const cors = require("cors");
 
 // Routes
 const homeRoutes = require("./routes/home");
 const authRoutes = require("./routes/auth");
 
-// Error Controller
-
 const app = express();
-
-// const whitelist = ["*"];
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use((req, res, next) => {
-//   const origin = req.get("referer");
-//   const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-//   if (isWhitelisted) {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Content-Type", "application/json");
-//     res.setHeader(
-//       "Access-Control-Allow-Methods",
-//       "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//     );
-//     res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "X-Requested-With,Content-Type,Authorization"
-//     );
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//   }
-//   // Pass to next layer of middleware
-//   if (req.method === "OPTIONS") res.sendStatus(200);
-//   else next();
-// });
-
-// const setContext = (req, res, next) => {
-//   if (!req.context) req.context = {};
-//   next();
-// };
-// app.use(setContext);
-
-// Set EJS
-// app.set("view engine", "ejs");
-// app.set("views", "./views");
-// app.use(express.static(path.join(__dirname, "public")));
-// app.use("/assets", express.static("assets"));
 
 app.use("/auth", authRoutes);
 
