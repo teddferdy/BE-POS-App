8a35d8d5a708302652c9c27d00c12a5b8b29aacf
Teddy-FN <tedd.ferdy@gmail.com>
2024-08-17 21:16:50 +0700

    Add Function Add Category

---
+++ db/migrations/20240817124407-create-category.js
@@ -0,0 +1,46 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('category', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        primaryKey: true
+      },
+      value: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.BOOLEAN
+      },
+      createdBy: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      modifiedAt: {
+        type: Sequelize.STRING
+      },
+      deletedAt: {
+        type: Sequelize.STRING
+      }
+    })
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('category')
+  }
+}
