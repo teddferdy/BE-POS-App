bf6d1eddb21582eb1dd1d399b43dc0845bdeb473
Teddy-FN <tedd.ferdy@gmail.com>
2024-09-03 13:00:42 +0700

    Fixing Image

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,47 +1,14 @@
 /* eslint-disable no-undef */
 const express = require('express')
-const multer = require('multer')
-const path = require('path')
+
 const router = express.Router()
 
 // Authorization
 const authorization = require('../../utils/authorization')
 const productController = require('../controller/product')
 
-const storage = multer.diskStorage({
-  destination: function (req, res, cb) {
-    let path = `/assets`
-    cb(null, path.join(__dirname + path))
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
-
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: '1000000'
-  },
-  fileFilter: (req, file, cb) => {
-    const fileTypes = /jpeg|jpg|png/
-    const mimeType = fileTypes.test(file.mimetype)
-    const extName = fileTypes.test(path.extname(file.originalname))
-
-    if (mimeType && extName) {
-      return cb(null, true)
-    }
-    cb('Give Proper files Formater to upload')
-  }
-}).single('image')
-
 // Post New Product
-router.post(
-  '/add-product',
-  upload,
-  authorization,
-  productController?.postAddProduct
-)
+router.post('/add-product', authorization, productController?.postAddProduct)
 
 router.get('/get-product', authorization, productController.getAllProduct)
 
