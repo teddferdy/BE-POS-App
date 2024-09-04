8ad269a9b790a06601277590fefa4141151f2a7b
Teddy-FN <tedd.ferdy@gmail.com>
2024-09-04 23:53:57 +0700

    FIxing Remove WA

--- api/routes/message.js
+++ api/routes/message.js
@@ -1,12 +1,12 @@
 const express = require('express')
 
 const router = express.Router()
-const whatsappClient = require('../service/whatsappClient')
+// const whatsappClient = require('../service/whatsappClient')
 
 // Post New Product
 
 router.post('/message', (req, res) => {
-  whatsappClient.sendMessage(req.body.phoneNumber, req.body.message)
+  // whatsappClient.sendMessage(req.body.phoneNumber, req.body.message)
   res.send()
 })
 
