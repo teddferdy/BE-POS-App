049e61ba70ae8f8d94a2dc2253f71d3645534e71
Teddy-FN <tedd.ferdy@gmail.com>
2024-09-07 14:45:36 +0700

    Adding Feature Edit / Delete Discount

--- api/controller/discount.js
+++ api/controller/discount.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 const Discount = require('../../db/models/discount')
 
-// Get All Sub-category
+// Get All Discount
 exports.getAllDiscount = async (req, res, next) => {
   try {
     const subCategory = await Discount.findAll()
@@ -29,17 +29,29 @@ exports.getAllDiscount = async (req, res, next) => {
 exports.postNewDiscount = async (req, res, next) => {
   try {
     const { description, percentage, isActive, createdBy } = req.body
-    const numbPercent = percentage.replace('%', '')
-    const postData = await Discount.create({
-      description: description,
-      percentage: parseFloat(numbPercent) / 100.0,
-      isActive: isActive,
-      createdBy: createdBy
+    const findOneDiscount = await Discount?.findOne({
+      where: {
+        nameStore: description
+      }
     })
-    return res.status(200).json({
-      status: 'success',
-      data: postData
-    })
+
+    if (!findOneDiscount?.getDataValue) {
+      const numbPercent = percentage.replace('%', '')
+      const postData = await Discount.create({
+        description: description,
+        percentage: parseFloat(numbPercent) / 100.0,
+        isActive: isActive,
+        createdBy: createdBy
+      })
+      return res.status(200).json({
+        status: 'success',
+        data: postData
+      })
+    } else {
+      return res.status(403).json({
+        message: 'Discount Sudah Terdaftar'
+      })
+    }
   } catch (error) {
     console.log('Error =>', error)
     return res.status(500).json({
@@ -48,5 +60,78 @@ exports.postNewDiscount = async (req, res, next) => {
   }
 }
 
-// Get Sub Category By idCategory
-exports.getSubCategory = async () => {}
+// Edit Discount By Id
+exports.editDiscountById = async (req, res, next) => {
+  const body = req.body
+  try {
+    const getDuplicate = await Discount.findOne({
+      where: {
+        description: body.description
+      }
+    })
+
+    if (!getDuplicate?.dataValues) {
+      const numbPercent = body.percentage.replace('%', '')
+      const editDiscount = await Discount?.update(
+        {
+          description: body.description,
+          percentage: parseFloat(numbPercent) / 100.0,
+          isActive: body.isActive,
+          createdBy: body.createdBy,
+          modifiedBy: body?.modifiedBy
+        },
+        {
+          returning: true,
+          where: {
+            id: body.id
+          }
+        }
+      ).then(([_, data]) => {
+        return data
+      })
+
+      return res.status(200).json({
+        message: 'Sukses Ubah Discount',
+        data: editDiscount?.dataValues
+      })
+    } else {
+      return res.status(403).json({
+        message: 'Discount Sudah Tersedia'
+      })
+    }
+  } catch (error) {
+    return res.status(500).json({
+      error: 'Terjadi Kesalahan Internal Server'
+    })
+  }
+}
+
+// Delete Discount By Id
+exports.deleteDiscountById = async (req, res, next) => {
+  const body = req.body
+
+  try {
+    const getId = await Location.destroy({
+      where: {
+        id: body.id,
+        description: body.description
+      },
+      force: true
+    })
+
+    if (getId) {
+      return res.status(200).json({
+        message: 'Success Hapus Discount'
+      })
+    } else {
+      return res.status(403).json({
+        message: 'Hapus Discount Gagal'
+      })
+    }
+  } catch (error) {
+    console.log('ERROR =>', error)
+    return res.status(500).json({
+      error: 'Terjadi Kesalahan Internal Server'
+    })
+  }
+}
