886a059612d1b7004d3fea7ddc5dcaa818cbabd3
Teddy-FN <tedd.ferdy@gmail.com>
2024-09-18 23:36:28 +0700

    Chart API

--- api/controller/best-selling.js
+++ api/controller/best-selling.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
 const BestSelling = require('../../db/models/best_selling')
+const Checkout = require('../../db/models/checkout')
+const { Op, Sequelize } = require('sequelize')
 
 // Get All List
 exports.getAllBestSelling = async (req, res, next) => {
@@ -16,6 +18,43 @@ exports.getAllBestSelling = async (req, res, next) => {
     return res.status(200).json({
       message: 'Success',
       data: getAllBestSelling?.length > 0 ? getAllBestSelling : []
+    })
+  } catch (error) {
+    return res.status(500).json({
+      error: 'Terjadi Kesalahan Internal Server'
+    })
+  }
+}
+
+// Cart
+exports.chartData = async (req, res, next) => {
+  const { query } = req
+  try {
+    const getAllBestSelling = await Checkout.findAll({
+      where: {
+        dateCheckout: {
+          [Op.between]: [query.startDate, query.endDate]
+        }
+      },
+      raw: true,
+      attributes: [
+        [Sequelize.literal(`DATE("dateCheckout")`), 'date'],
+        [Sequelize.literal(`COUNT(*)`), 'count']
+      ],
+      group: ['date']
+    }).then((res) => {
+      console.log('RES =>', res)
+      return res.map((items) => {
+        const getData = {
+          ...items
+        }
+        return getData
+      })
+    })
+
+    return res.status(200).json({
+      message: 'Success',
+      data: getAllBestSelling
     })
   } catch (error) {
     return res.status(500).json({
