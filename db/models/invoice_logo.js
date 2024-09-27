7ae99f5a8b55d11086b0edca25763bc73f88dd6c
Teddy-FN <tedd.ferdy@gmail.com>
2024-09-27 10:04:03 +0700

    Adding Api Invoice Logo

---
+++ db/models/invoice_logo.js
@@ -0,0 +1,48 @@
+'use strict'
+const { DataTypes } = require('sequelize')
+const sequelize = require('../../config/database')
+module.exports = sequelize.define(
+  'invoice_logo',
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    image: {
+      primaryKey: true,
+      type: DataTypes.STRING
+    },
+    isActive: {
+      primaryKey: true,
+      type: DataTypes.BOOLEAN
+    },
+    status: {
+      primaryKey: true,
+      type: DataTypes.BOOLEAN
+    },
+    createdBy: {
+      type: DataTypes.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    modifiedBy: {
+      type: DataTypes.STRING
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    deletedAt: {
+      type: DataTypes.STRING
+    }
+  },
+  {
+    paranoid: true,
+    freezeTableName: true,
+    modelName: 'invoice_logo'
+  }
+)
